bdcc9aff5096c6a2e8a0a741f167004575a21149
“davislr2” <davislr2@appstate.edu>
2023-10-20 14:04:40 -0400

    bug fixing

--- randScript.js
+++ randScript.js
@@ -1,20 +1,20 @@
 function pickBooneRestaurant() {
-    var options = { "Boone Saloon" : "0.5 Miles From Campus",
-                    "Macados" : "0.5 Miles From Campus",
-                    "Tapp Room" : "0.5 Miles From Campus",
-                    "Chick-Fil-A" : "2.0 Miles From Campus",
-                    "Cookout" : "1.0 Miles From Campus",
-                    "Mikes Inland Seafood" : "1.3 Miles From Campus",
-                    "Pedalin' Pig" : "3.1 Miles From Campus",
-                    "Los Arcoiris" : "2.2 Miles From Campus",
-                }
+    var options = { 
+        "Boone Saloon": "0.5 Miles From Campus",
+        "Macados": "0.5 Miles From Campus",
+        "Tapp Room": "0.5 Miles From Campus",
+        "Chick-Fil-A": "2.0 Miles From Campus",
+        "Cookout": "1.0 Miles From Campus",
+        "Mikes Inland Seafood": "1.3 Miles From Campus",
+        "Pedalin' Pig": "3.1 Miles From Campus",
+        "Los Arcoiris": "2.2 Miles From Campus",
+    }
     var keys = Object.keys(options)
     var randomIndex = Math.floor(Math.random() * keys.length)
     var randomKey = keys[randomIndex]
-    var priceRange = document.getElementById("priceRange").value
+    
+    var priceRange = document.getElementById("priceRange").value;
     document.getElementById("rangeBox").textContent = "Your chosen price range is: " + priceRange;
 
-    document.getElementById("resultBox").textContent 
-                        = "Your randomly chosen Boone restaurant is: "
-                        + randomKey + ". " + options[randomKey];
+    document.getElementById("resultBox").textContent = "Your randomly chosen Boone restaurant is: " + randomKey + ". " + options[randomKey];
 }
