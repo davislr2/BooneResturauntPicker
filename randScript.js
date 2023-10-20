a29cc5493e2c6c9762574a3ea78a798ad1c6dda8
“davislr2” <davislr2@appstate.edu>
2023-10-20 14:13:02 -0400

    added 1st version of functionality for price range

--- randScript.js
+++ randScript.js
@@ -1,15 +1,30 @@
 function pickBooneRestaurant() {
-    var options = { 
-        "Boone Saloon": "0.5 Miles From Campus",
-        "Macados": "0.5 Miles From Campus",
-        "Tapp Room": "0.5 Miles From Campus",
+    var cheap = { 
         "Chick-Fil-A": "2.0 Miles From Campus",
         "Cookout": "1.0 Miles From Campus",
-        "Mikes Inland Seafood": "1.3 Miles From Campus",
-        "Pedalin' Pig": "3.1 Miles From Campus",
         "Los Arcoiris": "2.2 Miles From Campus",
     }
-    var keys = Object.keys(options)
+
+    var moderate = {
+        "Tapp Room": "0.5 Miles From Campus",
+        "Macados": "0.5 Miles From Campus",
+        "Boone Saloon": "0.5 Miles From Campus",
+        "Ale House": "0.5 Miles From Campus"
+    }
+
+    var expensive = {
+        "Pedalin' Pig": "3.1 Miles From Campus",
+        "Mikes Inland Seafood": "1.3 Miles From Campus"
+    }
+
+    var optionsByRange = {
+        "cheap": cheap,
+        "moderate": moderate,
+        "expensive": expensive
+    }
+    var priceRange = document.getElementById("priceRange").value;
+
+    var keys = Object.keys(optionsByRange[priceRange])
     var randomIndex = Math.floor(Math.random() * keys.length)
     var randomKey = keys[randomIndex]
     
@@ -18,3 +33,4 @@ function pickBooneRestaurant() {
 
     document.getElementById("resultBox").textContent = "Your randomly chosen Boone restaurant is: " + randomKey + ". " + options[randomKey];
 }
+
