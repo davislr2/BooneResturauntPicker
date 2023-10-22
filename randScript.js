09a3e08556ae8ba7e5417919c0e40a7f24fc9432
“davislr2” <davislr2@appstate.edu>
2023-10-22 17:21:09 -0400

    typo in path name

--- randScript.js
+++ randScript.js
@@ -3,6 +3,7 @@ var shuffledOptions = [];
 var cheap = { 
     "Chick-Fil-A": "2.0 Miles From Campus",
     "Cookout": "1.0 Miles From Campus",
+    "Los Arcoiris": "2.2 Miles From Campus",
     "Taco Bell" : "0.9 Miles From Campus",
     "McDonalds" : "0.7 Miles From Campus",
     "Wendys" : "0.8 Miles From Campus",
@@ -15,6 +16,7 @@ var moderate = {
     "Macados": "0.5 Miles From Campus",
     "Boone Saloon": "0.5 Miles From Campus",
     "Ale House": "0.5 Miles From Campus",
+
     "Dos Amigos" : "1.1 Miles From Campus",
     "Los Arcoiris": "2.2 Miles From Campus"
 }
@@ -54,7 +56,7 @@ function pickBooneRestaurant() {
     document.getElementById("resultBox").textContent = 
                 "Your randomly chosen Boone restaurant is: " + randomKey + ". " + optionsByRange[priceRange][randomKey];
 
-    document.getElementById("restaurantImage").src = "images/" + randomKey + ".jpg";
+    document.getElementById("restaurantImage").src = "images/" + randomKey + ".jpeg";
 
 }
 
