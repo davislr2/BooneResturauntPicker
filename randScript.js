5ba0dcb7e1db486ec364389aacd532a1913304f2
“davislr2” <davislr2@appstate.edu>
2023-10-22 17:38:37 -0400

    had too many newlines and updated text incorrectly

--- randScript.js
+++ randScript.js
@@ -52,7 +52,7 @@ function pickBooneRestaurant() {
                  "Your chosen price range is: " + priceRange;
 
     document.getElementById("resultBox").textContent = 
-                "Your randomly chosen Boone restaurant is: \n" + randomKey + ". " + optionsByRange[priceRange][randomKey];
+                randomKey + ". " + optionsByRange[priceRange][randomKey];
 
     document.getElementById("restaurantImage").src = "images/" + randomKey + ".jpeg";
 
