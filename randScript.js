ee5f1dbc3ecb519caeecbc659f0e073213fe4331
“davislr2” <davislr2@appstate.edu>
2023-10-22 16:42:39 -0400

    got rid of the logic preventing repeat choices

--- randScript.js
+++ randScript.js
@@ -1,5 +1,41 @@
 var shuffledOptions = [];
 
+var cheap = { 
+    "Chick-Fil-A": "2.0 Miles From Campus",
+    "Cookout": "1.0 Miles From Campus",
+    "Los Arcoiris": "2.2 Miles From Campus",
+    "Taco Bell" : "0.9 Miles From Campus",
+    "McDonalds" : "0.7 Miles From Campus",
+    "Wendys" : "0.8 Miles From Campus",
+    "Bojangles" : "0.8 Miles From Campus",
+    "Chipotle" : "1.7 Miles From Campus",
+}
+
+var moderate = {
+    "Tapp Room": "0.5 Miles From Campus",
+    "Macados": "0.5 Miles From Campus",
+    "Boone Saloon": "0.5 Miles From Campus",
+    "Ale House": "0.5 Miles From Campus",
+    "Ale House": "0.5 Miles From Campus",
+    "Dos Amigos" : "1.1 Miles From Campus",
+    "Los Arcoiris": "2.2 Miles From Campus"
+}
+
+var expensive = {
+    "Pedalin' Pig": "3.1 Miles From Campus",
+    "Mikes Inland Seafood": "1.3 Miles From Campus",
+    "Casa Rustica": "1.2 Miles From Campus",
+    "Makatos" : "1.9 Miles From Campus",
+    "Mint" : "1.4 Miles From Campus",
+    "CoBo Sushi" : "1.1 Miles From Campus"
+}
+
+var optionsByRange = {
+    "Cheap": cheap,
+    "Moderate": moderate,
+    "Expensive": expensive
+}
+
 function randomizeArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -8,55 +44,12 @@ function randomizeArray(array) {
 }
 
 function pickBooneRestaurant() {
-    var cheap = { 
-        "Chick-Fil-A": "2.0 Miles From Campus",
-        "Cookout": "1.0 Miles From Campus",
-        "Los Arcoiris": "2.2 Miles From Campus",
-        "Taco Bell" : "0.9 Miles From Campus",
-        "McDonalds" : "0.7 Miles From Campus",
-        "Wendys" : "0.8 Miles From Campus",
-        "Bojangles" : "0.8 Miles From Campus",
-        "Chipotle" : "1.7 Miles From Campus",
-    }
 
-    var moderate = {
-        "Tapp Room": "0.5 Miles From Campus",
-        "Macados": "0.5 Miles From Campus",
-        "Boone Saloon": "0.5 Miles From Campus",
-        "Ale House": "0.5 Miles From Campus",
-        "Ale House": "0.5 Miles From Campus",
-        "Dos Amigos" : "1.1 Miles From Campus",
-        "Los Arcoiris": "2.2 Miles From Campus"
-    }
+    var priceRange = document.getElementById("priceRange").value;
+    var randomKey = ""; 
 
-    var expensive = {
-        "Pedalin' Pig": "3.1 Miles From Campus",
-        "Mikes Inland Seafood": "1.3 Miles From Campus",
-        "Casa Rustica": "1.2 Miles From Campus",
-        "Makatos" : "1.9 Miles From Campus",
-        "Mint" : "1.4 Miles From Campus",
-        "CoBo Sushi" : "1.1 Miles From Campus"
-    }
+    var randomKey = Object.keys(optionsByRange[priceRange])[Math.floor(Math.random() * Object.keys(optionsByRange[priceRange]).length)];
 
-    var optionsByRange = {
-        "Cheap": cheap,
-        "Moderate": moderate,
-        "Expensive": expensive
-    }
-    
-    if (!shuffledOptions || !shuffledOptions.length) {
-        priceRange = document.getElementById("priceRange").value;
-        shuffledOptions = Object.keys(optionsByRange[priceRange]);
-        randomizeArray(shuffledOptions);
-    }
-
-    if (!shuffledOptions.length) {
-        shuffledOptions = Object.keys(optionsByRange[priceRange]);
-        randomizeArray(shuffledOptions);
-    }
-
-    var randomKey = shuffledOptions.pop();
-    
     document.getElementById("rangeBox").textContent =
                  "Your chosen price range is: " + priceRange;
 
