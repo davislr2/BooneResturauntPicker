34c2d12386a3fecdb01324b4c59cc050a03f3cd1
“davislr2” <davislr2@appstate.edu>
2023-10-22 17:30:23 -0400

    removed Los from cheap list (already in moderate)

--- randScript.js
+++ randScript.js
@@ -3,7 +3,6 @@ var shuffledOptions = [];
 var cheap = { 
     "Chick-Fil-A": "2.0 Miles From Campus",
     "Cookout": "1.0 Miles From Campus",
-    "Los Arcoiris": "2.2 Miles From Campus",
     "Taco Bell" : "0.9 Miles From Campus",
     "McDonalds" : "0.7 Miles From Campus",
     "Wendys" : "0.8 Miles From Campus",
@@ -16,7 +15,6 @@ var moderate = {
     "Macados": "0.5 Miles From Campus",
     "Boone Saloon": "0.5 Miles From Campus",
     "Ale House": "0.5 Miles From Campus",
-
     "Dos Amigos" : "1.1 Miles From Campus",
     "Los Arcoiris": "2.2 Miles From Campus"
 }
